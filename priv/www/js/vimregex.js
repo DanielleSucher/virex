db51dd2b2846dea3720745ba8ac6da8b2bea1906
Danielle Sucher <dsucher@gmail.com>
2013-11-06 21:20:12 -0500

    Send ajax call on keyup

--- priv/www/js/vimregex.js
+++ priv/www/js/vimregex.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-  $('[data-behavior~=submit-regex-query]').keypress(function(e) {
+  $('[data-behavior~=submit-regex-query]').keyup(function(e) {
     e.preventDefault;
-    $.ajax("/highlight")
-      .done(function() {
-        alert( "success" );
-      })
-      .fail(function() {
-        alert( "error" );
-      })
+
+    test_string = $('#test_string').val();
+    regex = $('#regex').val();
+
+    if (regex && test_string) {
+      $.ajax("/highlight?text="+test_string+"&pattern="+regex)
+        .done(function(data) {
+          $('#match_result').html(data);
+        })
+        .fail(function() {
+          alert( "error" );
+        })
+    }
   });
 });
