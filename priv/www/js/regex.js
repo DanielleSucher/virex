4a027efceb8145498d398eb1974dee0b787da255
Danielle Sucher <dsucher@gmail.com>
2013-11-07 21:57:42 -0500

    Update with button instead of keyup

--- priv/www/js/regex.js
+++ priv/www/js/regex.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  $('[data-behavior~=submit-regex-query]').keyup(function(e) {
+  $('#update_regex').click(function(e) {
     e.preventDefault;
 
     test_string = $('#test_string').val();
     regex = $('#regex').val();
 
     if (regex && test_string) {
+      $('#match_result').css('background-color', '#EBEBEB');
+      $('#match_groups').css('background-color', '#EBEBEB');
+
       $.ajax("/regex?text="+test_string+"&pattern="+regex)
         .done(function(data) {
           $('#results').html(data);
+          $('#match_result').css('background-color', 'white');
+          $('#match_groups').css('background-color', 'white');
         });
     }
   });
