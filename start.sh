ec3cecd9f90f6f5d517379653fec142238ac0701
Danielle Sucher <dsucher@gmail.com>
2013-11-11 23:04:36 -0500

    Allow start script to take options
    So -detached can be passed in when started from init.d
    or really, generally on the server.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s virex
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s virex $VIREX_START_OPTIONS
